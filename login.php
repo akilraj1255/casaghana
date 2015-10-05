87dd97b2ea48208d6204fca73f58204d2ad1f242
iamkarsoft <karsoft92@gmail.com>
2015-10-05 18:48:05 +0000

    adding and modifying login, register, main css and header

---
+++ login.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+	<head><meta charset="utf-8">
+		<title>Casa Ghana</title>
+		<link href="css/style.css" rel="stylesheet">
+		 <link href="css/bootstrap.min.css" rel="stylesheet">
+	</head>
+<body>
+	
+	<div class="container">
+		<div class="login-form col-sm-12 col-md-4 col-md-offset-4">
+
+			<form class="">
+					<div class="form-group">
+						<input type="text" name="email" class="form-control">
+
+					</div>	 
+
+
+
+
+					<div class="form-group">
+						<input type="password" name="password" class="form-control">
+					</div>
+
+
+					<div class="form-group">
+						<button class="btn btn-primary">Login</button>
+					</div>
+			</form>
+
+		</div>
+	</div>
+		
+		
+	  
+	
+	
+	
+	</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="js/jquery-1.11.0.min.js" type="text/javascript"></script>
+   <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script  src="js/custom.js"></script>
