be7a226283a869fcd02cfbd52d084b25e1cdde6c
Kofi Amoussou <karsoft92@gmail.com>
2015-12-22 14:35:53 +0000

    fixed something

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,6 @@
 <?php
 
-$dbc=mysqli_connect('localhost','root','lollypop28','casa') or 
-$dbc= mysqli_connect('localhost','iamkarsoftdb','lollypop28','idamu_db') or die('Error Connecting to Database');
+$dbc=mysqli_connect('localhost','root','lollypop28','casa')  or die('Error Connecting to Database');
 
 
 ?>
