deadef23123f5d850998ca8a6d8be8b37ff1625b
Kofi Amoussou <karsoft92@gmail.com>
2015-12-25 14:26:58 +0000

    multiple database connection

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,20 @@
 <?php
 
-$dbc=mysqli_connect('localhost','root','lollypop28','casa')  or die('Error Connecting to Database');
+$dbc=mysqli_connect('localhost','root','lollypop28','casa');
+
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+
+
+/* change db to world db */
+mysqli_select_db($dbc, "idamu_db");
+
+
 
 
 ?>
