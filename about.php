50032f752ab9e6c5003d1bb80e0c59bbe12dbc18
Kofi Amoussou <karsoft92@gmail.com>
2015-12-28 21:58:43 +0000

    updated about page

--- about.php
+++ about.php
@@ -5,12 +5,8 @@
 	<div class="container ">
 		<div class="col-md-offset-2 col-md-8 property-single">
 			<h3 class="page-header">About Casa Ghana</h3>
-			<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+			<p> Rent, Buy or Sale  and even better share a property using Casa Ghana. We are trying to smoothing that process
+			with our classified portal that will allow users from the 10 regions to list their property online.</p>
 		</div>
 	
 	</div>
